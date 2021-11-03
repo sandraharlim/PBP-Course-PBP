4d03e994c6af62dd6c7e804644d7eca2de453229
Johanes Raka Triadana Nikaputra <johanes.raka@gmail.com>
2021-11-03 23:44:27 +0700

    Add Covid Data Module

--- covid_data/models.py
+++ covid_data/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class SearchedCovidData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    provinsi = models.CharField(max_length=30)
+    kasus_positif = models.CharField(max_length=10)
+    kasus_sembuh = models.CharField(max_length=10)
+    kasus_meninggal = models.CharField(max_length=10)
--- covid_data/views.py
+++ covid_data/views.py
@@ -3,57 +3,45 @@ from django.http import JsonResponse
 from django.core import serializers
 from django.template import loader
 from .forms import SearchForm
-#from .models import SearchedCovidData
+import requests
+from .models import SearchedCovidData
 
 
 def covid_data(request):
-    #searched_data = SearchedCovidData.objects.all()
+    searched_data = SearchedCovidData.objects.all()
     form = SearchForm(request.POST)
-    response = request.get('https://api.kawalcorona.com/indonesia/provinsi')
-    data = response.json()
+    response = requests.get('https://api.kawalcorona.com/indonesia/provinsi')
+    try:
+        data = response.json()
+    except:
+        data = None
     kasus = []
+    error = None
     searched_provinsi = None
     if request.user.is_authenticated:
         if request.method == 'POST':
             if form.is_valid():
                 searched_provinsi = form.cleaned_data['provinsi']
-    for o in data:
-        datanya = {}
-        datanya['provinsi'] = o['attributes']['Provinsi']
-        datanya['kasus_positif'] = o['attributes']['Kasus_Posi']
-        datanya['kasus_sembuh'] = o['attributes']['Kasus_Semb']
-        datanya['kasus_meninggal'] = o['attributes']['Kasus_Meni']
-        #if searched_provinsi != None:
-            #if datanya['provinsi'] == searched_provinsi:
-                #SearchedCovidData.objects.create(
-                   #provinsi=datanya['provinsi'], kasus_positif=datanya['kasus_positif'],kasus_sembuh=datanya['kasus_sembuh'],kasus_meninggal=datanya['kasus_meninggal']
-                   # )
-        kasus.append(datanya)
-
+    if data is not None:
+        for o in data:
+            datanya = {}
+            datanya['provinsi'] = o['attributes']['Provinsi']
+            datanya['kasus_positif'] = o['attributes']['Kasus_Posi']
+            datanya['kasus_sembuh'] = o['attributes']['Kasus_Semb']
+            datanya['kasus_meninggal'] = o['attributes']['Kasus_Meni']
+            if searched_provinsi != None:
+                if datanya['provinsi'] == searched_provinsi:
+                    SearchedCovidData.objects.create(
+                    provinsi=datanya['provinsi'], kasus_positif=datanya['kasus_positif'],kasus_sembuh=datanya['kasus_sembuh'],kasus_meninggal=datanya['kasus_meninggal']
+                        )
+            kasus.append(datanya)
+    else:
+        error = "Maaf sumber data yang kami pakai sedang bermasalah"
     return render(request, 'covid_data.html', {
-        'data': kasus,
         'searched_data': searched_data,
-        'form': form
+        'form': form,
+        'error': error,
         })
-
-#def process_covid_data(request):
-    # request should be ajax and method should be POST.
-    if request.is_ajax and request.method == "POST":
-        # get the form data
-        form = FriendForm(request.POST)
-        # save the data and after fetch the object in instance
-        if form.is_valid():
-            instance = form.save()
-            # serialize in new friend object in json
-            ser_instance = serializers.serialize('json', [ instance, ])
-            # send to client side.
-            return JsonResponse({"instance": ser_instance}, status=200)
-        else:
-            # some form errors occured.
-            return JsonResponse({"error": form.errors}, status=400)
-
-    # some error occured
-    return JsonResponse({"error": ""}, status=400)
 
 def search(request):
     response = requests.get('https://api.kawalcorona.com/indonesia/provinsi')
