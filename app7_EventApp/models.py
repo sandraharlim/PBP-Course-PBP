08aeb89e0b6da93be45299b425ac370909453322
NicoFathiRizqi <70448822+NicoFathiRizqi@users.noreply.github.com>
2021-11-04 14:54:13 +0700

    test push lagi

--- app7_EventApp/models.py
+++ app7_EventApp/models.py
@@ -6,8 +6,8 @@ class Event(models.Model):
     Name = models.CharField(max_length=30)
     Time = models.DateField()
     Description = models.TextField()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,
-        null=True, blank=True, on_delete=models.SET_NULL)
+    #author = models.ForeignKey(settings.AUTH_USER_MODEL,
+        #null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.Name
