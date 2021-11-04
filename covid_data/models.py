f1ae73e32328e5ebe8330c37dc7aa0cb482cf6db
Johanes Raka Triadana Nikaputra <johanes.raka@gmail.com>
2021-11-04 13:39:14 +0700

    Fix covid_data module

--- covid_data/models.py
+++ covid_data/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.conf import settings
 # Create your models here.
 
 class SearchedCovidData(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+        null=True, blank=True, on_delete=models.SET_NULL)
     provinsi = models.CharField(max_length=30)
     kasus_positif = models.CharField(max_length=10)
     kasus_sembuh = models.CharField(max_length=10)
--- covid_data/views.py
+++ covid_data/views.py
@@ -8,9 +8,9 @@ from .models import SearchedCovidData
 
 
 def covid_data(request):
-    searched_data = SearchedCovidData.objects.all()
-    form = SearchForm(request.POST)
-    response = requests.get('https://api.kawalcorona.com/indonesia/provinsi')
+    searched_data = SearchedCovidData.objects.all().values()
+    form = SearchForm(request.POST or None)
+    response = requests.get('https://data.covid19.go.id/public/api/prov_list.json')
     try:
         data = response.json()
     except:
@@ -23,17 +23,16 @@ def covid_data(request):
             if form.is_valid():
                 searched_provinsi = form.cleaned_data['provinsi']
     if data is not None:
-        for o in data:
+        for o in data['list_data']:
             datanya = {}
-            datanya['provinsi'] = o['attributes']['Provinsi']
-            datanya['kasus_positif'] = o['attributes']['Kasus_Posi']
-            datanya['kasus_sembuh'] = o['attributes']['Kasus_Semb']
-            datanya['kasus_meninggal'] = o['attributes']['Kasus_Meni']
+            datanya['provinsi'] = o['key']
+            datanya['kasus_positif'] = o['status']['buckets'][2]['doc_count']
+            datanya['kasus_sembuh'] = o['status']['buckets'][0]['doc_count']
+            datanya['kasus_meninggal'] = o['status']['buckets'][1]['doc_count']
             if searched_provinsi != None:
-                if datanya['provinsi'] == searched_provinsi:
-                    SearchedCovidData.objects.create(
-                    provinsi=datanya['provinsi'], kasus_positif=datanya['kasus_positif'],kasus_sembuh=datanya['kasus_sembuh'],kasus_meninggal=datanya['kasus_meninggal']
-                        )
+                if datanya['provinsi'] in searched_provinsi:
+                    new = SearchedCovidData(user = request.user,provinsi=datanya['provinsi'], kasus_positif=datanya['kasus_positif'],kasus_sembuh=datanya['kasus_sembuh'],kasus_meninggal=datanya['kasus_meninggal'])
+                    new.save()
             kasus.append(datanya)
     else:
         error = "Maaf sumber data yang kami pakai sedang bermasalah"
@@ -41,22 +40,28 @@ def covid_data(request):
         'searched_data': searched_data,
         'form': form,
         'error': error,
+        'date':data['last_date'],
         })
 
 def search(request):
-    response = requests.get('https://api.kawalcorona.com/indonesia/provinsi')
-    data = response.json()
-    kasus = []
-    for o in data:
-        datanya = {}
-        datanya['provinsi'] = o['attributes']['Provinsi']
-        datanya['kasus_positif'] = o['attributes']['Kasus_Posi']
-        datanya['kasus_sembuh'] = o['attributes']['Kasus_Semb']
-        datanya['kasus_meninggal'] = o['attributes']['Kasus_Meni']
-        kasus.append(datanya)
     # request should be ajax and method should be GET.
     if request.is_ajax and request.method == "GET":
         search_key = request.GET.get('search')
+        response = requests.get('https://data.covid19.go.id/public/api/prov_list.json')
+        data = response.json()
+        kasus = []
+        for o in data['list_data']:
+            datanya = {}
+            datanya['provinsi'] = o['key']
+            datanya['kasus_positif'] = o['status']['buckets'][2]['doc_count']
+            datanya['kasus_sembuh'] = o['status']['buckets'][0]['doc_count']
+            datanya['kasus_meninggal'] = o['status']['buckets'][1]['doc_count']
+            if request.user.is_authenticated:
+                if datanya['provinsi'] in search_key:
+                    new = SearchedCovidData(author = request.user,provinsi=datanya['provinsi'], kasus_positif=datanya['kasus_positif'],kasus_sembuh=datanya['kasus_sembuh'],kasus_meninggal=datanya['kasus_meninggal'])
+                    new.save()
+            kasus.append(datanya)
+        
         data_html = loader.render_to_string(
             'data.html',
             {'data': kasus, 'search_key': search_key}
