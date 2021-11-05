97386d9accf3b909b02b2273ed02d93804309462
zundullahbd <uluzundullah@gmail.com>
2021-11-06 01:29:10 +0900

    updating admin.py

--- questions/admin.py
+++ questions/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from django.db import models
 from .models import Question, Answer
 
 # Register your models here.
-class AnswerInline(admin.TabularInline):
-    model = Answer
 
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [AnswerInline]
-
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question)
 admin.site.register(Answer)
 
