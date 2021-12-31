a56b08c8a54d2be2b39b1a4e92b845df5dc445d4
Raihan Adliputra <raihanadliptr@gmail.com>
2021-12-31 19:18:51 +0700

    webservice foruumpandemi

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import add_info_pandemi, add_tips_kesehatan, add_curahan_hati, button, search_info_pandemi, search_curahan_hati, search_tips_kesehatan
+from .views import add_info_pandemi, add_tips_kesehatan, add_curahan_hati, button, search_info_pandemi, search_curahan_hati, search_tips_kesehatan, get_data, add_data_curahan_hati, add_data_info_pandemi, add_data_tips_kesehatan
 
 urlpatterns = [
     path('', button, name='button'),
@@ -10,4 +10,8 @@ urlpatterns = [
     path('search_info_pandemi', search_info_pandemi, name='search_info_pandemi'),
     path('search_tips_kesehatan', search_tips_kesehatan, name='search_tips_kesehatan'),
     path('search_curahan_hati', search_curahan_hati, name='search_curahan_hati'),
+    path('get_data', get_data, name='get_data'),
+    path('add_data_info_pandemi', add_data_info_pandemi, name='add_data_info_pandemi'),
+    path('add_data_curahan_hati', add_data_curahan_hati, name='add_data_curahan_hati'),
+    path('add_data_tips_kesehatan', add_data_tips_kesehatan, name='add_data_tips_kesehatan'),
 ]
