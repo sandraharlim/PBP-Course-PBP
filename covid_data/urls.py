50af714de1224af55b04ee0dd0f22ab19a21d67c
Johanes Raka Triadana Nikaputra <johanes.raka@gmail.com>
2021-12-31 18:11:16 +0700

    add backend for covid_data

--- covid_data/urls.py
+++ covid_data/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import covid_data, search
+from .views import covid_data, search, searchF, getModelsF
 
 urlpatterns = [
     path('', covid_data, name='covid_data'),
     path('data/', search, name='search'),
+    path('data/json', searchF, name='searchF'),
+    path('getmodels', searchF, name='getModelsF'),
 ]
